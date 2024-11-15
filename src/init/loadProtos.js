c65ce024b57b752bc1b2ac110be460bdf9f50b81
SSA <158470963+shinseongan@users.noreply.github.com>
2024-11-15 22:26:26 +0900

    Feat: 프로토버퍼 불러오기

--- src/init/loadProtos.js
+++ src/init/loadProtos.js
@@ -1,1 +1,41 @@
-// 프로토버퍼 불러오기
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import protobuf from 'protobufjs';
+import getAllProtoFiles from './protofiles.js';
+
+// 현재 파일의 절대경로
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// 상위 경로 + protobuf 폴더
+const protoDir = path.join(__dirname, '../protobuf');
+
+const protoFiles = getAllProtoFiles(protoDir);
+const protoMessages = {};
+
+export const loadProtos = async () => {
+  try {
+    const root = new protobuf.Root();
+
+    // 비동기 병렬 처리로 프로토 파일 로드
+    await Promise.all(protoFiles.map((file) => root.load(file)));
+
+    // packetNames 패킷들 등록
+    for (const [namespace, types] of Object.entries(packetNames)) {
+      protoMessages[namespace] = {};
+      for (const [type, typeName] of Object.entries(types)) {
+        protoMessages[namespace][type] = root.lookupType(typeName);
+      }
+    }
+
+    console.log('Protobuf 파일이 로드되었습니다.');
+  } catch (err) {
+    console.error('Protobuf 파일 로드 중 오류가 발생했습니다:', err);
+  }
+};
+
+export const getProtoMessages = () => {
+  // console.log('protoMessages:', protoMessages);
+
+  return { ...protoMessages };
+};
---
+++ src/init/protofiles.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+
+const getAllProtoFiles = (dir, fileList = []) => {
+  const files = fs.readdirSync(dir);
+
+  files.forEach((file) => {
+    const filePath = path.join(dir, file);
+
+    if (fs.statSync(filePath).isDirectory()) {
+      getAllProtoFiles(filePath, fileList);
+    } else if (path.extname(file) === '.proto') {
+      fileList.push(filePath);
+    }
+  });
+
+  return fileList;
+};
+
+export default getAllProtoFiles;

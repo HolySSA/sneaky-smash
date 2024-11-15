18375ea71ad4e112b7716f6ee9c8a1ad79a26081
SSA <158470963+shinseongan@users.noreply.github.com>
2024-11-15 22:50:47 +0900

    Feat: notification 패킷 생성

--- src/utils/notification/createNotification.js
+++ src/utils/notification/createNotification.js
@@ -1,1 +1,22 @@
-// Notification 패킷 생성
+import { getProtoMessages } from '../../init/loadProtos.js';
+import { config } from '../../config/config.js';
+import { PACKET_TYPE } from '../../constants/header.js';
+
+/**
+ * 패킷을 통해 버퍼 생성 후 전송
+ * @param {*} message
+ * @param {*} type
+ * @returns
+ */
+const makeNotification = (message, type) => {
+  const packetLength = Buffer.alloc(config.packet.totalLength);
+  packetLength.writeUInt32BE(
+    message.length + config.packet.totalLength + config.packet.typeLength,
+    0,
+  );
+
+  const packetType = Buffer.alloc(config.packet.typeLength);
+  packetType.writeUInt8(type, 0);
+
+  return Buffer.concat([packetLength, packetType, message]);
+};

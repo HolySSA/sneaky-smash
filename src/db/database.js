50c67586212eae1391002d8c2f985f349de9ef93
SSA <158470963+shinseongan@users.noreply.github.com>
2024-11-15 22:52:35 +0900

    Feat: db pool 생성

--- src/db/database.js
+++ src/db/database.js
@@ -1,1 +1,33 @@
-// db pool 생성
+import mysql from 'mysql2/promise';
+import { config } from '../config/config.js';
+import { formatDate } from '../utils/dateFormatter.js';
+
+const createPool = () => {
+  const pool = mysql.createPool({
+    //...config.databases.USER_DB,
+    //...config.databases.GAME_DB,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+
+  const originalQuery = pool.query;
+
+  pool.query = (sql, params) => {
+    const date = new Date();
+
+    console.log(
+      `[${formatDate(date)}] Executing query: ${sql} ${
+        params ? `, ${JSON.stringify(params)}` : ``
+      }`,
+    );
+
+    return originalQuery.call(pool, sql, params);
+  };
+
+  return pool;
+};
+
+const dbPool = createPool();
+
+export default dbPool;

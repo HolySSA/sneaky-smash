7f7519ebd0cbc178e7d8b87d42d3ceeedda8276c
803571 <8omsut@naver.com>
2024-11-18 16:48:25 +0900

    ✨ update: id생성, 중복id검증 추가

--- src/db/user/user.db.js
+++ src/db/user/user.db.js
@@ -1,5 +1,10 @@
 import dbPool from "../database.js";
 
+export const findUserByAccount = async () => {
+    const [rows] = await dbPool.USER.DB.query(SQL_QUERIES.FIND_USER_BY_ACCOUNT, [account]);
+    return rows[0].count > 0; // 찾는 아이디가 존재하면 true, 없으면 false
+}
+
 export const createUser = async (account, password) => {
     await dbPool.USER_DB.query(SQL_QUERIES.CREATE_USER, [account, password]);
   

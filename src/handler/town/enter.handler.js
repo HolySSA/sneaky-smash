6611b3e88f4021144a557c77fb7043d8f5dc2921
SSA <158470963+shinseongan@users.noreply.github.com>
2024-12-03 22:28:44 +0900

    Fix: 오류 수정

--- src/handler/town/enter.handler.js
+++ src/handler/town/enter.handler.js
@@ -1,14 +1,15 @@
 import handleError from '../../utils/error/errorHandler.js';
 import { createCharacter, findCharacterByUserId } from '../../db/model/characters.db.js';
-import { addRedisUser } from '../../sessions/redis/redis.user.js';
+import { addRedisUser, getRedisUserById } from '../../sessions/redis/redis.user.js';
 import { addUserSession } from '../../sessions/user.session.js';
 import enterLogic from '../../utils/etc/enter.logic.js';
 
 const enterHandler = async (socket, payload) => {
   try {
     addUserSession(socket);
-    const user = await addRedisUser(socket.id, payload.nickname, payload.class);
+    await addRedisUser(socket.id, payload.nickname, payload.class);
 
+    const user = await getRedisUserById(socket.id);
     const character = await findCharacterByUserId(parseInt(socket.id));
     if (!character) {
       // sql에서 gold default 선언해서 만들면 gold 입력 빼도 됨

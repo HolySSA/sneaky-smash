f05bc5cf1226535223352376106fe6a79a05a913
JungJaeU <wnsduq8737@nate.com>
2024-12-06 12:48:35 +0900

    [fix] 단순 오타 수정

--- src/handler/dungeon/hitPlayer.handler.js
+++ src/handler/dungeon/hitPlayer.handler.js
@@ -3,6 +3,7 @@ import { getDungeonSession } from '../../sessions/dungeon.session.js';
 import { getRedisUserById } from '../../sessions/redis/redis.user.js';
 import createResponse from '../../utils/response/createResponse.js';
 import handleError from '../../utils/error/errorHandler.js';
+import deathPlayerNotification from '../game/deathPlayer.notification.js';
 
 // message C_HitPlayer{
 //     int32 playerId = 1;  // 공격자 ID
@@ -19,15 +20,27 @@ const hitPlayerHandler = async (socket, payload) => {
     const redisUser = await getRedisUserById(playerId);
 
     const dungeon = getDungeonSession(redisUser.sessionId);
-
+    const victim = dungeon.getDungeonUser(playerId);
+    const attacker = dungeon.getDungeonUser(socket.id);
     const currentHp = dungeon.damagedUser(playerId, damage);
 
+    if (currentHp <= 0) {
+      const healAmount = attacker.statsInfo.stats.maxHp * 0.5;
+      attacker.currentHp = Math.min(
+        attacker.currentHp + healAmount,
+        attacker.statsInfo.stats.maxHp,
+        console.log(
+          `${attacker.userInfo.nickname}이가 ${victim.userInfo.nickname}을 처치하고 ${healAmount}회복했다`,
+        ),
+      );
+    }
     // updatePlayerHp 노티피케이션
     const updatePlayerHpResponse = createResponse(PACKET_ID.S_UpdatePlayerHp, {
       playerId,
       hp: currentHp,
     });
-
+    // 여기의 소켓은 공격자, playerId는 피격자.
+    if (currentHp <= 0) deathPlayerNotification(socket, playerId);
     const allUsers = dungeon.getAllUsers();
 
     const response = createResponse(PACKET_ID.S_HitPlayer, { playerId, damage });

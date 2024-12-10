e7cf0c8cebdaefc3fcd74358926be600ed5648cd
vxbdfsg <39340989+vxbdfsg@users.noreply.github.com>
2024-12-10 21:18:13 +0900

    add 채팅테스트
    add 채팅테스트

---
+++ src/tests/chat.test.js
@@ -0,0 +1,14 @@
+import Client from './client.test.js';
+import { getOrCreateClient } from './client.test.js';
+import configs from '../configs/configs.js';
+import testEnv from './env.test.js';
+import { PACKET_ID } from '../configs/constants/packetId.js';
+
+const client = getOrCreateClient(testEnv.url, testEnv.port);
+await client.connect();
+client.addHandler(PACKET_ID.S_Chat, async (payload) => {
+  // console.log(payload);
+});
+client.sendMessage(PACKET_ID.C_Chat, {
+    chatMsg: "하이~",
+});

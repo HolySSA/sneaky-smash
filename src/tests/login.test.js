1e3b86d561701d7b5f5a5ad6fbf521df4a4db875
ljs <artbiit@naver.com>
2024-12-10 16:26:51 +0900

    fix 로그인 테스트 코드가 로그인 수신할 때 까지 기다리도록 변경

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -7,17 +7,32 @@ const { PACKET_ID } = configs;
 const client = getOrCreateClient(testEnv.url, testEnv.port);
 await client.connect();
 
-client.addHandler(PACKET_ID.S_Login, async ({ payload }) => {
-  if (payload.success == true) {
-    client.token = payload.token;
-    client.expirationTime = payload.expirationTime;
-  } else {
-    client.token = undefined;
-    client.expirationTime = 0;
-  }
-});
+const handleLogin = () => {
+  return new Promise((resolve, reject) => {
+    client.addHandler(PACKET_ID.S_Login, async ({ payload }) => {
+      try {
+        if (payload.success === true) {
+          client.token = payload.token;
+          client.expirationTime = payload.expirationTime;
+        } else {
+          client.token = undefined;
+          client.expirationTime = 0;
+        }
+        resolve(payload); // 성공적으로 처리되면 resolve 호출
+      } catch (error) {
+        reject(error); // 에러 발생 시 reject 호출
+      }
+    });
+  });
+};
 
 client.sendMessage(PACKET_ID.C_Login, {
   account: testEnv.userName,
   password: testEnv.password,
 });
+
+try {
+  await handleLogin();
+} catch (error) {
+  console.error('Error during login:', error);
+}

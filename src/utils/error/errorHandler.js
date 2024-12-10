af7f16f1862766fd8bb509a2d6d828347bf02df0
ljs <artbiit@naver.com>
2024-12-10 16:08:35 +0900

    fix handleError가 서버를 죽게만드는 문제 해결

--- src/utils/error/errorHandler.js
+++ src/utils/error/errorHandler.js
@@ -1,31 +1,7 @@
-import createResponse from '../packet/createResponse.js';
 import logger from '../logger.js';
 
 const handleError = (socket, error) => {
-  let responseCode;
-  let message;
-
-  if (error.code) {
-    responseCode = error.code;
-    message = error.message;
-
-    logger.error(`에러 코드: ${error.code}, 메시지: ${error.message}`);
-  } else {
-    responseCode = 10000; // 일반 에러 코드
-    message = error.message;
-
-    logger.error(`일반 에러: ${error.message}`);
-  }
-
-  // 스택 트레이스 출력
-  if (error.stack) {
-    logger.error(`스택 트레이스:\n${error.stack}`);
-  } else {
-    logger.error('스택 트레이스 정보가 없습니다.');
-  }
-
-  const errorResponse = createResponse(-1, { message });
-  socket.write(errorResponse);
+  logger.error(error);
 };
 
 export default handleError;

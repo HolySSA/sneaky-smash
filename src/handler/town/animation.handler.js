00d77e70b8c4d0bcc957f2f9084a98044018eb6e
ljs <artbiit@naver.com>
2024-12-13 21:17:58 +0900

    fix 애니메이션 브로드캐스트에 세션 분류

--- src/handler/town/animation.handler.js
+++ src/handler/town/animation.handler.js
@@ -1,23 +1,31 @@
 import { PACKET_ID } from '../../configs/constants/packetId.js';
 import handleError from '../../utils/error/errorHandler.js';
-import { getAllUserUUID } from '../../sessions/user.session.js';
+import { getAllUserUUID, getUserById } from '../../sessions/user.session.js';
 import logger from '../../utils/logger.js';
 import Result from '../result.js';
+import { getDungeonSession } from '../../sessions/dungeon.session.js';
+import { getAllUserByTown } from '../../sessions/town.session.js';
 
 const animationHandler = async ({ socket, payload }) => {
   var animationPayload;
-  const allUsers = getAllUserUUID();
-  try {
-    const { animCode } = payload;
 
-    animationPayload = {
-      playerId: socket.id,
-      animCode,
-    };
-  } catch (e) {
-    handleError(socket, e);
+  const user = getUserById(socket.id);
+  let targetsUUID = null;
+
+  if (user.dungeonId) {
+    const dungeon = getDungeonSession(user.dungeonId);
+    targetsUUID = dungeon.usersUUID;
+  } else {
+    targetsUUID = getAllUserByTown();
   }
-  return new Result(animationPayload, PACKET_ID.S_Animation, allUsers);
+  const { animCode } = payload;
+
+  animationPayload = {
+    playerId: socket.id,
+    animCode,
+  };
+
+  return new Result(animationPayload, PACKET_ID.S_Animation, targetsUUID);
 };
 
 export default animationHandler;

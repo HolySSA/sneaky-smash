294d890d8b28885f0e321a681d5cdade387bcfd2
ljs <artbiit@naver.com>
2024-12-13 21:34:50 +0900

    fix 접속 세션 별로 브로드캐스트 구분하는 함수 추가

--- src/handler/town/animation.handler.js
+++ src/handler/town/animation.handler.js
@@ -5,27 +5,17 @@ import logger from '../../utils/logger.js';
 import Result from '../result.js';
 import { getDungeonSession } from '../../sessions/dungeon.session.js';
 import { getAllUserByTown } from '../../sessions/town.session.js';
+import broadcastBySession from '../../utils/notification/broadcastBySession.js';
 
 const animationHandler = async ({ socket, payload }) => {
   var animationPayload;
-
-  const user = getUserById(socket.id);
-  let targetsUUID = null;
-
-  if (user.dungeonId) {
-    const dungeon = getDungeonSession(user.dungeonId);
-    targetsUUID = dungeon.usersUUID;
-  } else {
-    targetsUUID = getAllUserByTown();
-  }
   const { animCode } = payload;
 
   animationPayload = {
     playerId: socket.id,
     animCode,
   };
-
-  return new Result(animationPayload, PACKET_ID.S_Animation, targetsUUID);
+  broadcastBySession(socket, PACKET_ID.S_Animation, animationPayload);
 };
 
 export default animationHandler;
--- src/handler/town/move.player.handler.js
+++ src/handler/town/move.player.handler.js
@@ -1,5 +1,11 @@
 import { PACKET_ID } from '../../configs/constants/packetId.js';
-import { getAllUserUUID, updateUserTransformById } from '../../sessions/user.session.js';
+import { getDungeonSession } from '../../sessions/dungeon.session.js';
+import {
+  getAllUserUUID,
+  getUserById,
+  updateUserTransformById,
+} from '../../sessions/user.session.js';
+import broadcastBySession from '../../utils/notification/broadcastBySession.js';
 import Result from '../result.js';
 /**
  * 무브 핸들러
@@ -7,7 +13,6 @@ import Result from '../result.js';
  * @param {object} payload - 클라이언트에서 전송한 데이터
  */
 const movePlayerHandler = async ({ socket, payload }) => {
-  const allUsers = getAllUserUUID().filter((uuid) => uuid !== socket.UUID);
   const { posX, posY, posZ, rot } = payload.transform;
   const transform = updateUserTransformById(socket.id, posX, posY, posZ, rot);
 
@@ -16,9 +21,7 @@ const movePlayerHandler = async ({ socket, payload }) => {
     transform: transform,
   };
 
-  if (allUsers.length > 0) {
-    return new Result(movePayload, PACKET_ID.S_Move, allUsers);
-  }
+  broadcastBySession(socket, PACKET_ID.S_Move, movePayload, true);
 };
 
 export default movePlayerHandler;

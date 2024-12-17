7489c5e78f6d96330488be2156448533691c19c2
ljs <artbiit@naver.com>
2024-12-18 02:49:57 +0900

    fix 서버 혼잡도 제어 추가

--- src/handler/user/logIn.handler.js
+++ src/handler/user/logIn.handler.js
@@ -1,13 +1,14 @@
 import jwt from 'jsonwebtoken';
 import { findCharacterByUserId } from '../../db/model/characters.db.js';
-import { addUserSession } from '../../sessions/user.session.js';
+import { addUserSession, getAllUserUUID } from '../../sessions/user.session.js';
 import enterLogic from '../../utils/etc/enter.logic.js';
 import configs from '../../configs/configs.js';
 import logger from '../../utils/logger.js';
 import createResponse from '../../utils/packet/createResponse.js';
 import { enqueueSend } from '../../utils/socket/messageQueue.js';
 
-const { PACKET_ID, JWT_SECRET, JWT_ALGORITHM, JWT_ISSUER, JWT_AUDIENCE } = configs;
+const { PACKET_ID, JWT_SECRET, JWT_ALGORITHM, JWT_ISSUER, JWT_AUDIENCE, SERVER_MAX_CAPACITY } =
+  configs;
 
 function verifyTokenAsync(token) {
   return new Promise((resolve, _) => {
@@ -32,26 +33,33 @@ const logInHandler = async ({ socket, payload }) => {
 
   let success = true;
   let message = undefined;
+  let existCharacter = false;
+  let character = null;
   try {
     const verified = await verifyTokenAsync(token);
     if (!verified) {
       success = false;
       message = '만료된 토큰 입니다.';
+    } else if (getAllUserUUID().length >= SERVER_MAX_CAPACITY) {
+      success = false;
+      message = '서버가 혼잡합니다.';
     } else {
       socket.id = Number(verified.id);
       socket.account = verified.account;
       addUserSession(socket);
-      const character = await findCharacterByUserId(socket.id);
-      const loginBuffer = createResponse(PACKET_ID.S_Authorize, {
-        success,
-        message,
-        existCharacter: character != null,
-      });
-      enqueueSend(socket.UUID, loginBuffer);
-      if (success) {
-        if (character) {
-          await enterLogic(socket, character);
-        }
+      character = await findCharacterByUserId(socket.id);
+      existCharacter = character != null;
+    }
+
+    const loginBuffer = createResponse(PACKET_ID.S_Authorize, {
+      success,
+      message,
+      existCharacter,
+    });
+    enqueueSend(socket.UUID, loginBuffer);
+    if (success) {
+      if (character) {
+        await enterLogic(socket, character);
       }
     }
   } catch (error) {
